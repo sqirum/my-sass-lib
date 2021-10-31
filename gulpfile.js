60daa377b9fc6d2ef1fc2e610da9606c57512216
sqirum <pauloslh94@gmail.com>
2021-10-31 22:38:12 +0000

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const {src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
     return src('sass/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({content: ['homepage.html']})) /* <--- para afetar todo o html ['*.html'] */
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['sass/**/*.scss'], buildStyles)
+    watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
